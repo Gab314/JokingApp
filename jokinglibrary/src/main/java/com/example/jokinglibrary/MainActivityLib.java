4663cfc522d930b4b2e3fa67ee0185d14dc15ee7
Gabe <gabriel.o.k@hotmail.com>
2017-10-05 13:24:19 -0700

    GCE mudolue connected, not working

--- jokinglibrary/src/main/java/com/example/jokinglibrary/MainActivityLib.java
+++ jokinglibrary/src/main/java/com/example/jokinglibrary/MainActivityLib.java
@@ -1,6 +1,8 @@
 package com.example.jokinglibrary;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -13,6 +15,9 @@ public class MainActivityLib extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+
 
         Joker joker = new Joker();
         joker.getJoke();
